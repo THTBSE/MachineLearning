05742e62e8968c917134b20c80b9aa19a5566d79
THTBSE <fuchilin90@gmail.com>
2015-02-06 17:07:03 +0800

    update ann

--- neural network/ann.py
+++ neural network/ann.py
@@ -6,6 +6,11 @@ def sigmoid(z):
 
 sigmoid_vec = np.vectorize(sigmoid)
 
+def sigmoid_prime(z):
+	return sigmoid(z) * (1 - sigmoid(z))
+
+sigmoid_prime_vec = np.vectorize(sigmoid_prime)
+
 class ann():
 	def __init__(self,sizes):
 		self.num_layers = len(sizes)
@@ -30,3 +35,41 @@ class ann():
 		else:
 			"Epoch {0} complete".format(j)
 	def update_mini_batch(self,mini_batch,eta):
+		nabla_b = [np.zeros(b.shape) for b in self.biases]
+		nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+		for x,y in mini_batch:
+			delta_nabla_b, delta_nabla_w = self.backprop(x,y)
+			nabla_b = [nb+dnb for nb,dnb in zip(nabla_b, delta_nabla_b)]
+			nabla_w = [nw+dnw for nw,dnw in zip(nabla_w, delta_nabla_w)]
+
+		self.weights = [w - (eta/len(mini_batch))*nw for w,nw in zip(self.weights, nabla_w)]
+		self.biases = [b - (eta/len(mini_batch))*nb for b,nb in zip(self.biases, nabla_b)]
+		
+	def backprop(self,x,y):
+		nabla_b = [np.zeros(b.shape) for b in self.biases]
+		nabla_w = [np.zeros(w.shape) for w in self.weights]
+		#feedforward
+		activation = x
+		activations = [x]
+		zs = []
+		for b,w in zip(self.biases, self.weights):
+			z = np.dot(w,activation) + b
+			zs.append(z)
+			activation = sigmoid_vec(z)
+			activations.append(activation)
+		#backpropagate
+		delta = self.cost_derivative(activations[-1],y) * sigmoid_prime_vec(zs[-1])
+		nabla_b[-1] = delta
+		nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+		for l in xrange(2,self.num_layers):
+			z = zs[-l]
+			spv = sigmoid_prime_vec(z)
+			delta = np.dot(self.weights[-l+1].transpose(), delta) * spv
+			nabla_b[-l] = delta
+			nabla_w[-l] = np.dot(delta,activations[-l-1].transpose())
+		return nabla_b,nabla_w
+
+	def cost_derivative(self, output_activations, y):
+		return (output_activations - y)
+
