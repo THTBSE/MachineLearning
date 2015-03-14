b6e53f11c3fc3a90d557df4d375ddacf35819ec7
Frank Fu <fuchilin90@gmail.com>
2015-03-14 15:37:47 +0800

    update LR

--- linearRegression/linearRegression.py
+++ linearRegression/linearRegression.py
@@ -1,6 +1,8 @@
 import numpy as np 
-from sklearn import linear_model
 
+__author__ = 'Frank'
+
+#for testing algorithm 
 def loadTrainData():
 	f = open('abalone.txt','rb')
 	train_data = []
@@ -10,6 +12,8 @@ def loadTrainData():
 		line = [float(x) for x in line]
 		train_data.append(line[:-1])
 		values.append(line[-1])
+	train_data = np.array(train_data)
+	values = np.array(values)
 	return train_data,values
 
 class SingularError(StandardError):
@@ -27,17 +31,16 @@ class LinearRegression():
 		return ((predict - y) ** 2).sum()
 
 	def predict(self,test_data):
-		if self.method == 'sgd':
-			result = np.dot(test_data,self.weights) + self.bias
-		else:
-			result = np.dot(test_data,self.weights)
+		result = np.dot(test_data,self.weights) + self.bias
 		return result
 
 	def __StandRegress(self,train_data,values):
 		xTx = np.dot(train_data.T,train_data)
 		if np.linalg.det(xTx) == 0.0:
 			raise SingularError('This matrix is singular, cannot do inverse')
-		self.weights = np.dot(np.dot(np.linalg.inv(xTx),train_data.T),values)
+		theta = np.dot(np.dot(np.linalg.inv(xTx),train_data.T),values)
+		self.weights = theta[:-1]
+		self.bias = theta[-1]
 
 	def __SGD(self,train_data,values):
 		"""
@@ -57,47 +60,61 @@ class LinearRegression():
 			print 'epoch {0}, diff is :{1}'.format(i,diff)
 
 	def fit(self,train_data,values):
-		if self.method != 'sgd':
-			for x in train_data:
-				x.append(1.0)
+		if self.method == 'std':
+			if isinstance(train_data,np.ndarray):
+				train_data = train_data.tolist()
+			[x.append(1.0) for x in train_data]
+		elif self.method == 'sgd':
+			pass
+		else:
+			raise ValueError('wrong method')
 		#convert list objects to numpy array
-		train_data = np.array(train_data)
+		if isinstance(train_data,list):
+			train_data = np.array(train_data)
+			values = np.array(values)
 		m,n = train_data.shape
-		values = np.array(values)
-
-		#self.weights = np.random.randn(n,)
-		self.weights = np.ones((n,))
-
+		self.weights = np.random.randn(n,)
+		#self.weights = np.ones((n,))
 		if self.method == 'sgd':
 			self.__SGD(train_data,values)
-		else:
+		elif self.method == 'std':
 			try:
 				self.__StandRegress(train_data,values)
 			except SingularError,e:
 				print e
 				self.__SGD(train_data,values)
-		print 'Weights are :{0} ,by {1} method'.format(self.weights,self.method)
-		print 'Bias is:{0}'.format(self.bias)
+		print 'Weights:{0}\n Bias:{1}\n computed by method:{2}'.format(self.weights,self.bias,self.method)
 
-train_data,y = loadTrainData()
-lr = LinearRegression(method='std')
-lr.fit(train_data,y)
+if __name__ == '__main__':
+	train_data,y = loadTrainData()
+	lr = LinearRegression(method='std')
+	lr.fit(train_data,y)
 
-py = lr.predict(train_data)
-error = lr.error(py,y)
-print 'STD error is:{0}'.format(error)
+	py = lr.predict(train_data)
+	error = lr.error(py,y)
+	print 'STD error is:{0}'.format(error)
 
-train_data1,y1 = loadTrainData()
-lrSGD = LinearRegression()
-lrSGD.fit(train_data1,y1)
+	train_data1,y1 = loadTrainData()
+	lrSGD = LinearRegression()
+	lrSGD.fit(train_data1,y1)
 
-pySGD = lrSGD.predict(train_data1)
-errorSGD = lrSGD.error(pySGD,y1)
-print 'SGD error is:{0}'.format(errorSGD)
+	pySGD = lrSGD.predict(train_data1)
+	errorSGD = lrSGD.error(pySGD,y1)
+	print 'SGD error is:{0}'.format(errorSGD)
 
-clf = linear_model.LinearRegression()
-clf.fit(train_data1,y1)
-pyCLF = clf.predict(train_data1)
-errorpyCLF = lr.error(pyCLF,y1)
-print clf.coef_
-print errorpyCLF
+	from sklearn import linear_model
+	clf = linear_model.LinearRegression()
+	clf.fit(train_data1,y1)
+	pyCLF = clf.predict(train_data1)
+	errorpyCLF = lr.error(pyCLF,y1)
+	print 'Standard by sklearn'
+	print clf.coef_
+	print errorpyCLF
+
+	clfSGD = linear_model.SGDRegressor()
+	clfSGD.fit(train_data1,y1)
+	pyCLF1 = clfSGD.predict(train_data1)
+	errorpyCLF1 = lr.error(pyCLF1,y1)
+	print 'SGD by sklearn'
+	print clfSGD.coef_
+	print errorpyCLF1
